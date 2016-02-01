fe137b35a3996ca272b976f61350bb8a2b7fd63a
Stuart Urback <stuart@urback.net>
2016-02-01 09:16:17 -0700

    Add jquery click functions

--- js/app.js
+++ js/app.js
@@ -4,6 +4,14 @@ dragula([document.querySelector('nav'), document.querySelector('main')],{
   },
   accepts: function(el, target){
     return target.tagName === 'MAIN' ? true : false;
-  }
+  },
+  removeOnSpill: true,
 
+})
+
+$('main').on('click', '.close', function(){
+  var getRidOf = confirm('Do you want to remove this element?')
+  if (getRidOf){
+    $(this).parent().remove();
+  }
 })
